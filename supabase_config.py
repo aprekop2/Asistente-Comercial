fceabf348b04dd456b6358896388400fe3b9846c
aprekop2 <153242753+aprekop2@users.noreply.github.com>
2025-04-21 16:19:13 -0500

    supabase_config.py

--- supabase_config.py
+++ supabase_config.py
@@ -1,4 +1,4 @@
-from supabase import create_client
+from supabase import create_client, Client
 import os
 from dotenv import load_dotenv
 
@@ -7,3 +7,11 @@ load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+def guardar_interaccion(usuario, mensaje, respuesta):
+    data = {
+        "usuario": usuario,
+        "mensaje": mensaje,
+        "respuesta": respuesta,
+    }
+    supabase.table("interacciones").insert(data).execute()
